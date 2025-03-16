44854e087fcfff1a8a89a1cdc25efac5ffd19a4e
clarissaboll <98230713+clarissaboll@users.noreply.github.com>
2025-03-17 00:35:25 +0100

    Update analytical_data.R

--- code/analytical_data.R
+++ code/analytical_data.R
@@ -1,9 +1,13 @@
 process_analytical_files <- function(conn) {
+  # Retrieve all file names related to analytical data
   analyticalFiles <- get_file_names(filePath, "analytical")
   
+  # Create necessary tables in the database
   create_chemical_table(conn)
   create_analyticaldata_table(conn)
   create_measurementdata_table(conn)
+  
+  # Process each analytical file
   for (file in analyticalFiles) {
     analytical_data <- prepare_analytical_data(file)
     chemical_ids <- insert_chemicals(conn, analytical_data)
@@ -13,8 +17,8 @@ process_analytical_files <- function(conn) {
 }
 
 
-
 prepare_analytical_data <- function(file) {
+  # Get all sheet names and the file as a tidyxl tibble
   full_file_path <- file.path(filePath, file)
   allsheetnames <- excel_sheets(full_file_path)
   chemicals_cell_info <- get_cell_infos(full_file_path, allsheetnames)
@@ -22,6 +26,7 @@ prepare_analytical_data <- function(file) {
   sheet_data_list <- list()
   
   for (sheet_name in allsheetnames) {
+    # Extract column positions for all headers in the current sheet 
     cas_no_col <- find_col_by_keyword(chemicals_cell_info, sheet_name, "cas no")
     ec_no_col <- find_col_by_keyword(chemicals_cell_info, sheet_name, "ec no")
     category_col <- find_col_by_keyword(chemicals_cell_info, sheet_name, "category")
@@ -30,7 +35,7 @@ prepare_analytical_data <- function(file) {
     sample_col <- find_col_by_keyword(chemicals_cell_info, sheet_name, "sample")
     c_no_col <- find_col_by_keyword(chemicals_cell_info, sheet_name, "^c n")
     
-    
+    # Identify rows that contain actual data (not headers)
     analytical_rows <- chemicals_cell_info %>%
       filter(is_blank == FALSE) %>%
       filter(sheet == sheet_name) %>%
@@ -38,6 +43,7 @@ prepare_analytical_data <- function(file) {
       distinct(row) %>%
       pull(row) 
     
+    # Add all header columns and the data rows to the analyticaldata list for later use
     sheet_data_list[[sheet_name]] <- list(
       cas_no_col = cas_no_col,
       ec_no_col = ec_no_col,
@@ -58,31 +64,30 @@ prepare_analytical_data <- function(file) {
 }
 
 insert_chemicals <- function(conn, analytical_data) {
+  # Extract the relevant data from the prepared data 
   chemicals_cell_info <- analytical_data$chemicals_cell_info
   sheet_names <- analytical_data$allsheetnames
-  
   all_chemicals <- list()
   
   for (sheet_name in sheet_names) {
+    # Extract the relevant data for chemicals from the prepared data 
     analytical_rows <- analytical_data$sheet_data[[sheet_name]]$analytical_rows
     cas_no_col <- analytical_data$sheet_data[[sheet_name]]$cas_no_col
     ec_no_col <- analytical_data$sheet_data[[sheet_name]]$ec_no_col
     substance_col <- analytical_data$sheet_data[[sheet_name]]$substance_col
     category_col <- analytical_data$sheet_data[[sheet_name]]$category_col
+    
+    # Go through each filled row 
     for (row_num in analytical_rows) {
-      cas_no <- filter_sheet_row_col(chemicals_cell_info, sheet_name, row_num, cas_no_col[1])%>%
-        pull(character)
-
-      ec_no <- filter_sheet_row_col(chemicals_cell_info, sheet_name, row_num, ec_no_col[1])%>%
-        pull(character)
+      # Extract chemical information from each row 
+      cas_no <- filter_sheet_row_col(chemicals_cell_info, sheet_name, row_num, cas_no_col[1]) %>% pull(character)
+      ec_no <- filter_sheet_row_col(chemicals_cell_info, sheet_name, row_num, ec_no_col[1]) %>% pull(character)
+      substance_name <- filter_sheet_row_col(chemicals_cell_info, sheet_name, row_num, substance_col[1]) %>% pull(character)
+      category_name <- filter_sheet_row_col(chemicals_cell_info, sheet_name, row_num, category_col[1]) %>% pull(character)
       
-      substance_name <- filter_sheet_row_col(chemicals_cell_info, sheet_name, row_num, substance_col[1])%>%
-        pull(character)
-      
-      category_name <- filter_sheet_row_col(chemicals_cell_info, sheet_name, row_num, category_col[1])%>%
-        pull(character)
-      
+      # Check if CAS and EC Number are not null, since they have NOT NULL condition 
       if (!is.null(cas_no) && !is.null(ec_no)) {
+        # Add the data for one row to the list for later batch insertion 
         all_chemicals <- append(all_chemicals, list(
           list(
             cas_no = cas_no,
@@ -94,10 +99,12 @@ insert_chemicals <- function(conn, analytical_data) {
       }
     }
   }
+  
+  # Insert chemicals into database and return their IDs
   chemical_df <- bind_rows(all_chemicals)
   chemical_ids <- insert_chemicals_batch(conn, chemical_df)
   return(chemical_ids)
-  }
+}
 
 insert_analyticalData <- function(conn, analytical_data, chemical_ids) {
   chemicals_cell_info <- analytical_data$chemicals_cell_info
@@ -163,49 +170,57 @@ get_sample_id <- function(df, chemicalid, method_name, c_no) {
 }
 
 insert_measurementData <- function(conn, analytical_data, sample_ids, chemical_ids) {
+  # Extract the relevant data from the prepared data 
   chemicals_cell_info <- analytical_data$chemicals_cell_info
   chemicalsheetnames <- analytical_data$allsheetnames
   all_measurements <- list()
+  
   for (sheet_name in chemicalsheetnames) {
+    # Extract the relevant data for measurementdata from the prepared data 
     ec_no_col <- analytical_data$sheet_data[[sheet_name]]$ec_no_col
     c_no_col <- analytical_data$sheet_data[[sheet_name]]$c_no_col
-  
+    
+    # Retrieve all measurement units present in the current sheet 
     measurement_units <- get_measurement_units(chemicals_cell_info, sheet_name, "wt%|mg/kg")
-
+    
+    # Go trough all measurement unit columns 
     for (j in 1:nrow(measurement_units)) {
       current_col <- measurement_units$col[j]
       current_unit <- measurement_units$character[j]
+      
+      # Retrieve the values for all rows for the current measurement unit 
       measurement_data <- get_measurement_data(chemicals_cell_info, sheet_name, current_col)
-     
+      
+      # Go through all rows of the measurement values for the current unit
       for (k in 1:nrow(measurement_data)) {
         current_row <- measurement_data$row[k]
-        ec_number <- filter_sheet_row_col(chemicals_cell_info, sheet_name, current_row, ec_no_col[1])%>%
-          pull(character)
+        # Retrieve values necessary for the foreign key sample id 
+        ec_number <- filter_sheet_row_col(chemicals_cell_info, sheet_name, current_row, ec_no_col[1]) %>% pull(character)
         c_no <- get_c_number_from_col(chemicals_cell_info, sheet_name, current_row, c_no_col[1])
-      
-          if (length(c_no) == 0) {
-            c_no <- NA
-          }
-         
-         chemical_id <- chemical_ids %>%
-          filter(ec_no == ec_number) %>%
-          pull(chemicalid)
-      
-         sampleid <- get_sample_id(sample_ids, chemical_id, sheet_name, c_no);
-         all_measurements <- append(all_measurements, list(
-           list(
-             sample_id = sampleid,
-             measurement = current_unit,
-             value = measurement_data$numeric[k]
-           )
-         ))
+        if (length(c_no) == 0) { c_no <- NA }
+        # Get the foreign key 
+        chemical_id <- chemical_ids %>% filter(ec_no == ec_number) %>% pull(chemicalid)
+        sampleid <- get_sample_id(sample_ids, chemical_id, sheet_name, c_no)
+        
+        # Add the current row to the data list for later batch insertion 
+        all_measurements <- append(all_measurements, list(
+          list(
+            sample_id = sampleid,
+            measurement = current_unit,
+            value = measurement_data$numeric[k]
+          )
+        ))
       }
     }
   }
+  
+  # Insert all measurement data into database
   measurement_df <- bind_rows(all_measurements)
   dbWriteTable(conn, "measurementData", as.data.frame(measurement_df), append = TRUE, row.names = FALSE)
 }
 
+# Retreives the c Number value and handles special cases where C Number is numeric, 
+# character or doesn't exist
 get_c_number_from_col <- function(chemicals_cell_info, sheet_name, row, c_no_col) {
   return(filter_sheet_row_col(chemicals_cell_info, sheet_name, row, c_no_col[1]) %>%
            mutate(
