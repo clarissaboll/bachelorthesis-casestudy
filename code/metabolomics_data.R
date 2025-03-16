6246793feca4cfec0bb21329ce06d1fbe797b7a0
clarissaboll <98230713+clarissaboll@users.noreply.github.com>
2025-03-16 23:54:13 +0100

    Update metabolomics_data.R

--- code/metabolomics_data.R
+++ code/metabolomics_data.R
@@ -1,5 +1,3 @@
-filePath <- here("data")
-
 process_metabolomics_files <- function(conn) {
   metabolomicsFiles <- get_file_names(filePath, "metabolom")
   create_metabolomics_table(conn)
@@ -13,7 +11,6 @@ insert_metabolomics <- function(file, conn) {
   full_file_path <- file.path(filePath, file)
   allsheetnames <- excel_sheets(full_file_path)
   metabolomics_cells_info <- get_cell_infos(full_file_path, allsheetnames)
-  print("checkpoint1")
   all_metabolomics <- list()
   
   for (sheet_name in allsheetnames) {
@@ -37,49 +34,35 @@ insert_metabolomics <- function(file, conn) {
       pull(col)
     metabolite_col <- get_header_position(metabolome_cells_info, sheet_name, "metabolite") %>% 
       pull(col)
-    print("checkpoint2")
+    
     filled_rows <- get_filled_rows(metabolome_cells_info, sheet_name, starting_row + 1)
-    print("checkpoint2.01")
+ 
     filled_cols <- get_filled_cols(metabolome_cells_info, sheet_name, 3)
     
     analyte_info <- get_analyte_info(metabolome_cells_info, sheet_name, filled_rows, analyte_id_col, metabolite_col, analyte_id_row, metabolite_row)
     animal_ids <- get_animal_ids(conn)
-    print("checkpoint2.1")
+ 
     for (i in seq_along(filled_cols)) {
       current_col <- filled_cols[i]
       
       substudy <- filter_character_from_row_col(metabolome_cells_info, sheet_name, substudy_row, current_col)
       animal_name <- filter_character_from_row_col(metabolome_cells_info, sheet_name, animal_id_row, current_col)
-      print("checkpoint2.2")
+      
       animalid <- animal_ids %>%
         filter(animalname == animal_name) %>%
         pull(animalid)
-      print("checkpoint2.3")
+     
       aliasid <- filter_sheet_row_col(metabolome_cells_info, sheet_name, alias_id_row, current_col) %>%
         pull(numeric) %>%
         as.integer()
-      print("checkpoint3")
+    
       for (current_row in filled_rows) {
-        
-        if ("analyte_id" %in% colnames(analyte_info)) {
-          analyteid <- analyte_info %>%
-            filter(row == current_row) %>%
-            pull(analyte_id)
-        } else {
-          analyteid <- NA 
-        }
-        
-        if ("metabolite_name" %in% colnames(analyte_info)) {
-          metabolitename <- analyte_info %>%
-            filter(row == current_row) %>%
-            pull(metabolite_name)
-        } else {
-          metabolitename <- NA
-        }
+        analyteid <- get_value_from_analyte_info(analyte_info, "analyte_id", current_row)
+        metabolitename <- get_value_from_analyte_info(analyte_info, "metabolite_name", current_row)
         
         value <- filter_sheet_row_col(metabolome_cells_info, sheet_name, current_row, current_col) %>%
           pull(numeric)
-        print("checkpoint4")
+       
         all_metabolomics <- append(all_metabolomics, list(
           list(
             animal_id = animalid,
@@ -101,6 +84,16 @@ insert_metabolomics <- function(file, conn) {
   }
 }
 
+get_value_from_analyte_info <- function(analyte_info, col_name, current_row) {
+  if (col_name %in% colnames(analyte_info)) {
+          value <- analyte_info %>%
+            filter(row == current_row) %>%
+            pull(!!sym(col_name))
+        } else {
+          value <- NA
+        }
+  return(value)
+}
 
 get_header_position <- function(metabolome_cells_info, Metabolomesheetname, name) {
   return(metabolome_cells_info %>%
@@ -151,7 +144,6 @@ get_analyte_info <- function(metabolome_cells_info, current_sheet, filled_rows, 
   }
   analyte_info <- analyte_info %>%
     rename_with(~ column_mappings[.x], names(column_mappings))
-  
   
   return(analyte_info)
 }
